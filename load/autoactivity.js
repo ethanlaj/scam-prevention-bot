aefade6a7817edda4711f18a36903dcb9c72fb6f
ethanlaj <ethanlaj@ericlaj.com>
2018-06-03 16:05:59 -0400

    a

--- load/autoactivity.js
+++ load/autoactivity.js
@@ -2,15 +2,11 @@ var messages = [
 	"Message 1",
 	"Message 2",
 	"Message 3",
-	"Message 4",
-	"Message 5"
 ];
 var activities = [
 	"WATCHING",
 	"PLAYING",
 	"LISTENING",
-	"WATCHING",
-	"PLAYING"
 ];
 //activities correspond w/ messages
 module.exports.run = async (bot) => {
@@ -19,6 +15,7 @@ module.exports.run = async (bot) => {
 		if(!bot.auto) return;
 		if (loopNumber === messages.length - 1) loopNumber = 0;
 		bot.user.setActivity(messages[loopNumber], { type: activities[loopNumber] }).catch(function () { });
+		console.log(messages[loopNumber]);
 		loopNumber++;
 	}, 2000);
 };
