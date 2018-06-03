f6d791aa4d5e87c4321dcb1e015a04ff7ccff899
ethanlaj <ethanlaj@ericlaj.com>
2018-06-03 17:43:51 -0400

    Update serverjoinandleave.js

--- load/serverjoinandleave.js
+++ load/serverjoinandleave.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
-//const DBL = require("dblapi.js");
-//const request = require("request-promise-native");
-/*function postServerCount() {
+const DBL = require("dblapi.js");
+const request = require("request-promise-native");
+function postServerCount() {
 	return request.post({
 		uri: `https://discordbots.org/api/bots/${bot.user.id}/stats`,
 		headers: {
@@ -12,9 +12,9 @@ const Discord = require("discord.js");
 			server_count: bot.guilds.size,
 		},
 	});
-}*/
+}
 module.exports.run = async (bot) => {
-	//postServerCount()
+	postServerCount()
 	bot.on("guildCreate", async guild => {
 		//postServerCount()
 		let welcomeMessage = new Discord.RichEmbed().setTitle("Thanks For Adding Me To Your Server!").setColor("#0000ff").setDescription("Thanks for inviting Scam reports bot to your server!\nScam reports bot is owned by Scooby and ethan and was made by the Co-Owner, @ethanlaj#8805. For a list of commands, just say `!help`\nIf you need any help what so ever, feel free to join our support server!\nInvite link: https://discord.gg/53Jgcu5");
@@ -22,6 +22,6 @@ module.exports.run = async (bot) => {
 		if (hichannel) hichannel.send(welcomeMessage);
 	});
 	bot.on("guildDelete", async () => {
-		//postServerCount()
+		postServerCount()
 	});
 };
