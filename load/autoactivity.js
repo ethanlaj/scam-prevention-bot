3b0a30ffc79121503ba9ca9e497c471bbe15a912
ethanlaj <ethanlaj@ericlaj.com>
2018-06-03 16:30:03 -0400

    a

--- load/autoactivity.js
+++ load/autoactivity.js
@@ -1,21 +1,44 @@
 var messages = [
+	"{users} users",
+	"for !help",
+	"on {servers} servers",
+	"for !updates",
+	"premium for only $3/month",
+	"for !buypremium",
+	"for scammers",
 	"for !report",
-	"Watching for !updates",
-	"for !updates"
+	"on {channels} channels",
+	"for !invite",
 ];
 var activities = [
+	"LISTENING",
+	"WATCHING",
+	"PLAYING",
+	"WATCHING",
+	"LISTENING",
+	"WATCHING",
+	"LISTENING",
 	"WATCHING",
 	"PLAYING",
 	"WATCHING",
 ];
+
+/*variables:
+{users} = user count
+{servers} = server count
+*/
+
 //activities correspond w/ messages
 module.exports.run = async (bot) => {
 	var loopNumber = 0;
 	bot.setInterval(function() {
 		if(!bot.auto) return;
 		if (loopNumber === messages.length) loopNumber = 0;
-		bot.user.setActivity(messages[loopNumber], { type: activities[loopNumber] }).catch(function () { });
+		var message = messages[loopNumber];
+		message = message.replace("{users}", bot.users.size);
+		message = message.replace("{servers}", bot.guilds.size);
+		bot.user.setActivity(message, { type: activities[loopNumber] }).catch(function () { });
 		console.log(messages[loopNumber]);
 		loopNumber++;
-	}, 30000);
+	}, 2000);
 };
