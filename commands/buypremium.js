a52cc8a8b9e2511acbc2beb2b45a0ee687b2abe5
noanimeallowed <35615829+scoobycode@users.noreply.github.com>
2018-06-03 18:21:32 -0700

    Update buypremium.js

--- commands/buypremium.js
+++ commands/buypremium.js
@@ -1,6 +1,6 @@
 
 module.exports.run = async (bot, message) => {
-	message.reply("**You can buy premium here!:** https://selly.gg/p/3442c00e").catch(() => {
+	message.reply("**You can buy premium here!:** https://selly.gg/p/3b0483db").catch(() => {
 		return message.author.send(`You attempted to use the \`buypremium\` command in ${message.channel}, but I can not chat there.`).catch(function () { });
 	});
 };
