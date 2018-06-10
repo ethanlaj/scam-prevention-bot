397a7d5cd038635f836472db3d1f1c151c3f9f73
Yikey <gtc122004@gmail.com>
2018-06-10 12:15:03 -0400

    some efficiency updates

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const botconfig = require("./botconfig.js");
 const Discord = require("discord.js");
 const fs = require("fs");
-const bot = new Discord.Client({disableEveryone: true, fetchAllMembers: true});
+const bot = new Discord.Client({ disableEveryone: true, fetchAllMembers: true });
 bot.loaders = { enabledLoaders: [], disabledLoaders: [] };
-bot.data = { prefixes: [], inPrompt: [], blacklistedUsers: [], blacklistedGuilds: [], scammers: [], codes: [], pusers: [], timeout: []};
+bot.data = { prefixes: [], inPrompt: [], blacklistedUsers: [], blacklistedGuilds: [], scammers: [], codes: [], pusers: [], timeout: [] };
 bot.auto = false;
 bot.commands = new Discord.Collection();
 bot.disabledCommands = [];
@@ -12,7 +12,7 @@ fs.readdirSync(__dirname + "/load").forEach(file => {
 	try {
 		let loader = require("./load/" + file);
 		bot.loaders.enabledLoaders.push(loader);
-	} catch(err) {
+	} catch (err) {
 		bot.loaders.disabledLoaders.push(file);
 		console.log(`\nThe ${file} load module failed to load:`);
 		console.log(err);
@@ -41,7 +41,7 @@ fs.readdir("./commands/", (err, files) => {
 			} else {
 				throw checkCommand(props, f)[1];
 			}
-		} catch(err) {
+		} catch (err) {
 			bot.disabledCommands.push(f);
 			console.log(`\nThe ${f} command failed to load:`);
 			console.log(err);
@@ -59,62 +59,69 @@ bot.on("ready", () => {
 bot.on("message", async message => {
 	if (!message.author.bot && message.channel.type !== "dm") {
 		var args = message.content.split(" "),
-		    cmd = args.shift().toLowerCase();
-	}
-	var messageArray = message.content.split(" ");
-	var cmd = messageArray[0].toLowerCase();
-	var args = messageArray.slice(1);
-	var rawPrefix = bot.data.prefixes.find(value => value.guild === message.guild.id);
-	var prefix = (rawPrefix != null) ? rawPrefix.prefix : botconfig.prefix;
-	
-	if (bot.data.pusers.find(value => value.id === message.author.id) && bot.data.pusers.find(value => value.id === message.author.id).expires !== "0" && bot.data.pusers.find(value => value.id === message.author.id).expires < Date.now()) {
-		bot.data.pusers.find(value => value.id === message.author.id).msg.delete();
-		bot.data.pusers.splice(bot.data.pusers.indexOf(bot.data.pusers.find(value => value.id === message.author.id)), 1);
-		message.author.send("Your premium has expired!").catch(function () { });
-	}
-	let guild = bot.guilds.find("id", "443867131721941005");
-	var permissionLevel = 0;
-	if (guild.members.get(message.author.id)) {
-		var member = await guild.fetchMember(message.author.id);
-		if (member.roles) {
-			if (member.roles.get("443903247502147596")) permissionLevel = 1;
-			if (member.roles.get("443898332029517824")) permissionLevel = 2;
-			if (member.roles.get("443867603103121410")) permissionLevel = 3;
+			cmd = args.shift().toLowerCase();
+		var rawPrefix = bot.data.prefixes.find(value => value.guild === message.guild.id);
+		var prefix = (rawPrefix != null) ? rawPrefix.prefix : botconfig.prefix;
+
+		if (bot.data.pusers.find(value => value.id === message.author.id) && bot.data.pusers.find(value => value.id === message.author.id).expires !== "0" && bot.data.pusers.find(value => value.id === message.author.id).expires < Date.now()) {
+			bot.data.pusers.find(value => value.id === message.author.id).msg.delete();
+			bot.data.pusers.splice(bot.data.pusers.indexOf(bot.data.pusers.find(value => value.id === message.author.id)), 1);
+			message.author.send("Your premium has expired!").catch(function() {});
 		}
-	}
-	//0 = Non-Member or Non-Matching Roles
-	//1 = Moderators
-	//2 = Helper
-	//3 = Developers
-	if (message.content.startsWith(prefix)) {
-		let commandfile = bot.commands.get(cmd.slice(prefix.length));
-		if (!commandfile) return;
-		if(bot.data.timeout.find(value => value.id === message.author.id)) return message.reply("You cannot use this command yet!").catch(() => {
-			return message.author.send(`You attempted to use a command in ${message.channel}, but I can not chat there.`).catch(function () { });
-		});
-		bot.setTimeout(function() {
-			bot.data.timeout.splice(bot.data.timeout.indexOf(bot.data.timeout.find(value => value.id === message.author.id)), 1);
-		}, 2000);
-		bot.data.timeout.push({ id: message.author.id });
-		return commandfile.run(bot, message, args, prefix, permissionLevel);
-	} else if (message.content.startsWith(`<@!${bot.user.id}>`) || message.content.startsWith(`<@${bot.user.id}>`)) {
-		let commandfile = bot.commands.get(args[0].toLowerCase());
-		if (!commandfile) return;
-		if (message.content.startsWith(`<@${bot.user.id}>`)) {
-			message.content = message.content.replace(`<@${bot.user.id}> `, `${prefix}`);
-		} else if (message.content.startsWith(`<@!${bot.user.id}>`)) {
-			message.content = message.content.replace(`<@!${bot.user.id}> `, `${prefix}`);
+		let guild = bot.guilds.find("id", "443867131721941005");
+		var permissionLevel = 0;
+		if (guild.members.get(message.author.id)) {
+			var member = await guild.fetchMember(message.author.id);
+			if (member.roles) {
+				if (member.roles.get("443903247502147596")) permissionLevel = 1;
+				if (member.roles.get("443898332029517824")) permissionLevel = 2;
+				if (member.roles.get("443867603103121410")) permissionLevel = 3;
+			}
 		}
-		messageArray = message.content.split(" ");
-		args = messageArray.slice(1);
-		if(bot.data.timeout.find(value => value.id === message.author.id)) return message.reply("You cannot use this command yet!").catch(() => {
-			return message.author.send(`You attempted to use a command in ${message.channel}, but I can not chat there.`).catch(function () { });
-		});
-		bot.setTimeout(function() {
-			bot.data.timeout.splice(bot.data.timeout.indexOf(bot.data.timeout.find(value => value.id === message.author.id)), 1);
-		}, 2000);
-		bot.data.timeout.push({ id: message.author.id });
-		return commandfile.run(bot, message, args, prefix, permissionLevel);
+		//0 = Non-Member or Non-Matching Roles
+		//1 = Moderators
+		//2 = Helper
+		//3 = Developers
+		var commandFile;
+		if (message.content.startsWith(prefix)) {
+			commandFile = bot.commands.get(cmd.slice(prefix.length));
+			if (commandFile != null) {
+				var timeout = bot.data.timeout.find(value => value.id === message.author.id);
+				if (timeout == null) {
+					bot.setTimeout(() => {
+						bot.data.timeout.splice(bot.data.timeout.indexOf(timeout), 1);
+					}, 2000);
+					bot.data.timeout.push({ id: message.author.id });
+					commandFile.run(bot, message, args, prefix, permissionLevel);
+				} else {
+					message.reply("You cannot use this command yet!").catch(() => {
+						message.author.send(`You attempted to use a command in ${message.channel}, but I can not chat there.`).catch(function() {});
+					});
+				}
+
+			}
+		} else if (message.content.startsWith(`<@!${bot.user.id}>`) || message.content.startsWith(`<@${bot.user.id}>`)) {
+			commandFile = bot.commands.get(args[0].toLowerCase());
+			if (commandFile != null) {
+				if (message.content.startsWith(`<@${bot.user.id}>`)) {
+					message.content = message.content.replace(`<@${bot.user.id}> `, prefix);
+				} else if (message.content.startsWith(`<@!${bot.user.id}>`)) {
+					message.content = message.content.replace(`<@!${bot.user.id}> `, prefix);
+				}
+				if (bot.data.timeout.find(value => value.id === message.author.id) == null) {
+					bot.setTimeout(() => {
+						bot.data.timeout.splice(bot.data.timeout.indexOf(bot.data.timeout.find(value => value.id === message.author.id)), 1);
+					}, 2000);
+					bot.data.timeout.push({ id: message.author.id });
+					commandFile.run(bot, message, args, prefix, permissionLevel);
+				} else {
+					message.reply("You cannot use this command yet!").catch(() => {
+						message.author.send(`You attempted to use a command in ${message.channel}, but I can not chat there.`).catch(function() {});
+					});
+				}
+
+			}
+		}
 	}
 });
 bot.login(botconfig.token);
